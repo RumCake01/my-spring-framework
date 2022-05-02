a5ea7b76644506d625c36c9aa40e96c2096300ac
RumCake01 <mia.hasnutdinova@gmail.com>
2022-05-02 17:36:13 -0400

    tightly coupled

---
+++ Spring-00-Coupling/src/main/java/com/cydeo/tightly_coupled/CydeoApp.java
@@ -0,0 +1,13 @@
+package com.cydeo.tightly_coupled;
+
+public class CydeoApp {
+    public static void main(String[] args) {
+
+        FulltTimeMentor ft = new FulltTimeMentor();
+        PartTimeMentor pt = new PartTimeMentor();
+
+        Mentor mentor = new Mentor(ft, pt );
+        mentor.manageAccount();
+
+    }
+}
