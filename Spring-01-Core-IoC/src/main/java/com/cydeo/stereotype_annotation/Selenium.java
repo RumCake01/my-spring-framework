5641803d0f5c65f800e1b7a0e814483b9ca0345b
RumCake01 <mia.hasnutdinova@gmail.com>
2022-05-02 18:11:03 -0400

    Stereotype Annotation - @Component @Component Scan

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/stereotype_annotation/Selenium.java
@@ -0,0 +1,11 @@
+package com.cydeo.stereotype_annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Selenium {
+
+        public void getTeachingHours() {
+            System.out.println("Total Selenium teaching hours: 30");
+        }
+}
