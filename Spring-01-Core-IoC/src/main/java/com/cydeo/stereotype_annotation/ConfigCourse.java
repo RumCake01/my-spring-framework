438be269b089951cbbdcb0f887db0a3c73c45dc7
RumCake01 <mia.hasnutdinova@gmail.com>
2022-05-02 18:13:11 -0400

    @ComponentScan(basePackages = "com.cydeo")- looks up beans under com.cydeo packages

--- Spring-01-Core-IoC/src/main/java/com/cydeo/stereotype_annotation/ConfigCourse.java
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/stereotype_annotation/ConfigCourse.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+// @ComponentScan
+@ComponentScan(basePackages = "com.cydeo") // looks up beans in matching files under com.cydeo packages
 
 public class ConfigCourse {
 
