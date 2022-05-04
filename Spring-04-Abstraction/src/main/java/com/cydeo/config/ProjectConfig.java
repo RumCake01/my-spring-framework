afa89f4bae410f4223a8bb59ad17c85bb60a9857
RumCake01 <mia.hasnutdinova@gmail.com>
2022-05-04 18:27:16 -0400

    @ComponentScan(basePackages={"com.cydeo.proxy", "com.cydeo.service", "com.cydeo.repository"}) added to create beans from other packages

--- Spring-04-Abstraction/src/main/java/com/cydeo/config/ProjectConfig.java
+++ Spring-04-Abstraction/src/main/java/com/cydeo/config/ProjectConfig.java
@@ -1,9 +1,15 @@
 package com.cydeo.config;
 
+import com.cydeo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"com.cydeo.config", "com.cydeo.service", "com.cydeo.repository"})
+@ComponentScan
+@ComponentScan(basePackages={"com.cydeo.proxy", "com.cydeo.service", "com.cydeo.repository"})
 public class ProjectConfig {
+
+
 }
